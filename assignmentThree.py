5278d036cf4c8d9e928bb8bbe83493682f098d47
ZADO1 <ziad.ghattass33@gmail.com>
2023-10-29 23:01:37 +0200

    add assignment 3 part 8

--- assignmentThree.py
+++ assignmentThree.py
@@ -80,4 +80,12 @@ def matrix_input():  # O(n^2)
             row.append(value)
         list.append(row)
     return list
-
+#
+# part8
+def palindrome(s):
+    if len(s) <= 1:
+        return True
+    if s[0] == s[-1]:
+        return palindrome(s[1:-1])
+    else:
+        return False
