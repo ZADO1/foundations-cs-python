99cc3e8d16c787991f7d40aff0c5455a36fa77f1
ZADO1 <ziad.ghattass33@gmail.com>
2023-11-09 22:17:28 +0200

    create closing tab

--- MidtermProject.py
+++ MidtermProject.py
@@ -10,10 +10,41 @@ def displayMenu():
           "7.Save Tabs\n",
           "8.Import Tabs\n",
           "9.Exit\n",)
-
+tabs = []
 def openTab():
     title = input("Enter The Title of the Website:")
     url = input("Enter the URl:")
+    tab = {'title': title, 'url': url}      #i use dictionary
+    tabs.append(tab)
+    main()
+
+def CloseTab():
+    print("tab  you have opened ",tabs)
+    if not tabs:
+        print("No tabs to close")
+    index = input("Enter the Index of the tab you wish to close: ")
+    tabs.pop(index)
+
+
+
+
+
+
+    if index =='':
+        index = -1
+
+    else:
+        index = int(index)
+        if index < -1 or index >= len(tabs):
+            print("Invalid index.")
+    main()
+
+
+
+
+
+    # main()
+
 
 
 
@@ -22,6 +53,13 @@ def main():
     choice = input("Enter Your Choice:")
     if choice == "1":
         openTab()
+
+    elif choice =="2":
+        CloseTab()
+
+
+
+
 
 
 main()
