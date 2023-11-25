d1ecb7dd6dbb4648a18bf9e706f6024fa206d98f
ZADO1 <ziad.ghattass33@gmail.com>
2023-11-25 23:02:11 +0200

    create first menu

---
+++ assignment4.py
@@ -0,0 +1,7 @@
+def menu():
+    print("1.Singly linkedlist\n"+
+          "2.Check if palindrom\n"+
+          "3.Priority Queue\n"+
+          "4.Evaluate an Infix Expression\n"+
+          "5.Graph\n"+
+          "6.Exit")
