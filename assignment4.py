287535e43c7813b5528af8bb5523fdeb0a270dd4
ZADO1 <ziad.ghattass33@gmail.com>
2023-11-25 23:25:43 +0200

    define class linkedlist and class node

--- assignment4.py
+++ assignment4.py
@@ -12,3 +12,18 @@ def linkedlistMenu():
           "b.Display Nodes\n"+
           "c.Search for & Delete Node\n"+
           "d.Return to main menu")
+
+# we need to define class of linked list ,every linkedlist has a head and tail
+# we put head  and tail equal none because when w create linkedlist will be empty
+class Linkedlist:
+    def __int__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+# linkedlist consists of nodes  we need to create class node
+# node containe data  and next we put next equal to none by default
+class Node:
+    def __int__(self,data):
+        self.data= data
+        self.next = None
